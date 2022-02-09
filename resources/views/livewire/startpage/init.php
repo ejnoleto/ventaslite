e04e0d666c63147d9ef1059ecf72cddd325ea8cc
ejnoleto <contato@emmanuelnoleto.com.br>
2022-02-09 14:01:52 -0300

    ajustes para sair da VM

---
+++ resources/views/livewire/startpage/init.php
@@ -0,0 +1,44 @@
+<div class="row sales layout">
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+            <div class="widget-heading">
+                <h4 class="card-title">
+                    <b>ComponentName | PageTitle</b>
+                </h4>
+                <ul class="tabs tab-pills">
+                    <li>
+                        <a href="javascript:void(0)" class="tabmenu bg-dark" data-toggle="modal" data-target="#">
+                            Agregar
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            Search
+            <div class="widget-content">
+                <div class="table-responsive">
+                    <table class="table table-bordered table striped mt-1">
+                        <thead class="text white" style="background:#3b3f5c">
+                            <tr>
+                                <th class="table-th text-white">DESCRIPCION</th>
+                                <th class="table-th text-white">IMAGEN</th>
+                                <th class="table-th text-white">ACTIONS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <h6>CATEGORY NAME</h6>
+                                </td>
+                                <td class="text-center">
+                                    <span>
+                                        <img src="" alt="Imagem de exemplo" height="70" srcset="">
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        </ div>
+    </div>
