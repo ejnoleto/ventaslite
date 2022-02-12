47a374894350f20b7541dbb490cad124dbe84011
ejnoleto <contato@emmanuelnoleto.com.br>
2022-02-12 19:07:00 -0300

    alterações para funcionar categories

--- resources/views/livewire/category/categories.blade.php
+++ resources/views/livewire/category/categories.blade.php
@@ -29,7 +29,7 @@
                             @foreach ($categories as $category)
                             <tr>
                                 <td>
-                                    <h6>{{ $category ?? ''->name }}</h6>
+                                    <h6>{{ $category->name }}</h6>
                                 </td>
                                 <td class="text-center">
                                     <span>
